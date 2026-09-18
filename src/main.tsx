81a50c7a17a98f0dc42ea06247926a48503c474f
Seprayanh <seprayanh07@outlook.com>
2026-09-18 20:02:00 +0800

    Integrate PINWISE typography system

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App'
+import './styles/design-tokens.css'
 import './index.css'
 import './v3-overrides.css'
 import './v3_1-overrides.css'
